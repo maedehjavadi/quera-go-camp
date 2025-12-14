8d9bd05a109b541832c78179e4f300cb99cd15f2
maedeh javadi <maedeh.j@behinrahkar.com>
2025-12-14 22:49:06 +0330

    first change in first practice 2

--- 1.lebase_habib/main.go
+++ 1.lebase_habib/main.go
@@ -7,6 +7,7 @@ import (
 	"strings"
 )
 
+// This is main function
 func readInputLines(scanner *bufio.Scanner) ([]string, map[string][]string) {
 	lines := make([]string, 0, 6)           // length = 0, capacity = 6
 	categories := make(map[string][]string) // category: colors list[]
